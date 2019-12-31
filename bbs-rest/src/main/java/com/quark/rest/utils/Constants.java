9529dc983642af14ef503f81199223696489eab1
lithiumed <823724@gmail.com>
2019-12-31 17:57:13 +0800

    适配本地环境

--- bbs-rest/src/main/java/com/quark/rest/utils/Constants.java
+++ bbs-rest/src/main/java/com/quark/rest/utils/Constants.java
@@ -1,12 +1,18 @@
 package com.quark.rest.utils;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+
 /**
  * @Author ycw
  * Create By 2017/8/22
  */
+
 public class Constants {
+
+
     //Upload常量
-    public static final String UPLOAD_PATH = "D:\\home\\images\\upload\\";
-    public static final String STATIC_URL ="http://127.0.0.1/images/upload/";
+    public static final String UPLOAD_PATH = "/Users/lithiumed/Downloads/";
+    public static final String STATIC_URL = "http://127.0.0.1:8086/images/upload/";
 
 }
